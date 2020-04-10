e2359a723a4a1120cb62921d4115e711dbd90daf
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-04-11 00:00:00 +0900

    "2020-04-11 00:00"

--- template-parts/pageLoginTemplate/single-content-styles.php
+++ template-parts/pageLoginTemplate/single-content-styles.php
@@ -18,6 +18,27 @@
   padding: 10px 30px;
 }
 .u-section-1 .u-btn-2 {
+  border-style: none none solid;
+  align-self: center;
+  margin: 30px 0 0;
+  padding: 0;
+}
+.u-section-1 .u-btn-3 {
+  border-style: none none solid;
+  margin: 30px 0 0;
+  padding: 0;
+}
+@media (max-width: 1199px) {
+  .u-section-1 .u-btn-2 {
+    margin-left: 185px;
+  }
+  .u-section-1 .u-btn-3 {
+    margin-right: 185px;
+    margin-bottom: 60px;
+  }
+}
+@media (max-width: 991px) {
+  .u-section-1 .u-btn-2 {
     margin-left: 75px;
   }
   .u-section-1 .u-btn-3 {
