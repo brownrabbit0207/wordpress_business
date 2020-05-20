7995bad0809e1d15ae6e5be46ccda18f8047235b
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-05-21 00:00:00 +0900

    "2020-05-21 00:00"

--- template-parts/postTemplate/single-content.php
+++ template-parts/postTemplate/single-content.php
@@ -8,7 +8,8 @@
                             $post_image = theme_get_post_image(array('class' => 'u-blog-control u-expanded-width u-image u-image-default u-image-1', 'default' => '/images/0fd3416c.jpeg'));
                             if ($post_image) echo $post_image; else { echo '<div class="none-post-image" style="display: none;"></div>'; $skip_min_height = true; } ?><!--/blog_post_image--><!--blog_post_header-->
         <h2 class="u-blog-control u-text u-text-1">
-    </div><!--/blog_post--><!--/post_details-->
-  </div>
-</section><?php if ($skip_min_height) { echo "<style> .u-section-1, .u-section-1 .u-sheet {min-height: auto;}</style>"; } ?>
-<?php } ?>
+          <?php if (!is_singular()): ?><a class="u-post-header-link" href="<?php the_permalink(); ?>"><?php endif; ?><?php the_title(); ?><?php if (!is_singular()): ?></a><?php endif; ?>
+        </h2><!--/blog_post_header--><!--blog_post_metadata-->
+        <div class="u-blog-control u-metadata u-metadata-1"><!--blog_post_metadata_date-->
+          <span class="u-meta-date u-meta-icon"><?php if(is_search() || $post->post_type !== "page"){ echo get_the_date(); }?></span><!--/blog_post_metadata_date--><!--blog_post_metadata_category-->
+          <?php $categories = theme_get_category_list(); if ($categories):?><span class="u-meta-category u-meta-icon"><?php echo $categories; ?></span><?php endif; ?><!--/blog_post_metadata_category--><!--blog_post_metadata_comments-->
