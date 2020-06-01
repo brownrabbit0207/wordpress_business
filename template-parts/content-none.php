4757c63dc5c300bfd93ab7c8d2454bfb4e00ccea
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-06-02 00:00:00 +0900

    "2020-06-02 00:00"

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -3,12 +3,6 @@
  * The template part for displaying a message that posts cannot be found
  */
 ?>
-
-<section class="no-results not-found">
-	<header class="page-header">
-		<h2 class="page-title"><?php _e( 'Nothing Found', 'website4829605' ); ?></h2>
-	</header><!-- .page-header -->
-
 	<div class="page-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
@@ -23,3 +17,9 @@
 
 		<?php else : ?>
 
+			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'website4829605' ); ?></p>
+			<?php get_search_form(); ?>
+
+		<?php endif; ?>
+	</div><!-- .page-content -->
+</section><!-- .no-results -->
