db623bb48ba7185e2cd7465e36192af2a7171e29
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-08-05 00:00:00 +0900

    "2020-08-05 00:00"

--- template-parts/blogTemplate/blog-content-styles.php
+++ template-parts/blogTemplate/blog-content-styles.php
@@ -8,21 +8,36 @@
   margin-bottom: 60px;
   height: auto;
 }
-.u-section-1 .u-repeater-1 {
-  grid-template-columns: repeat(3, calc(33.3333% - 14.6667px));
-  min-height: 441px;
-  grid-gap: 22px;
-}
-.u-section-1 .u-repeater-item-1 {
-  background-image: none;
-}
-.u-section-1 .u-container-layout-1 {
-  padding: 30px 20px;
-}
 .u-section-1 .u-text-1 {
   margin-top: 0;
   margin-bottom: 0;
 }
+.u-section-1 .u-image-1 {
+  height: 222px;
+  margin-top: 17px;
+  margin-bottom: 0;
+  visibility: hidden;
+}
+.u-section-1 .u-text-2 {
+  margin-top: 20px;
+  margin-bottom: 0;
+}
+.u-section-1 .u-btn-1 {
+  background-image: none;
+  border-style: solid;
+  margin: 17px auto 0 0;
+  padding: 0;
+}
+.u-section-1 .u-container-layout-2 {
+  padding: 30px 20px;
+}
+.u-section-1 .u-text-3 {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+.u-section-1 .u-image-2 {
+  height: 222px;
+  margin-top: 17px;
   margin-bottom: 0;
   visibility: hidden;
 }
