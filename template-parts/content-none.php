4a5a601d50cc22cd2be5c601702037719557c395
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-09-19 00:00:00 +0900

    "2020-09-19 00:00"

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -13,3 +13,13 @@
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'website4829605' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+
+		<?php elseif ( is_search() ) : ?>
+
+			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'website4829605' ); ?></p>
+            </br>
+            <?php get_search_form(); ?>
+            </br>
+
+		<?php else : ?>
+
