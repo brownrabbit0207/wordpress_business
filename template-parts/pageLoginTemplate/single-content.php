a03d37a282a8c6427a3a5eb53c1856f5a079180b
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-10-21 00:00:00 +0900

    "2020-10-21 00:00"

--- template-parts/pageLoginTemplate/single-content.php
+++ template-parts/pageLoginTemplate/single-content.php
@@ -3,6 +3,8 @@
     <div class="u-form u-login-control u-form-1">
       <?php global $pageLogin_custom_template;
 	    $pathToTemplates = get_template_directory() . '/template-parts/' . $pageLogin_custom_template;
-    
-  </div>
-</section><?php if ($skip_min_height) { echo "<style> .u-section-1, .u-section-1 .u-sheet {min-height: auto;}</style>"; } ?>
+	    $pathToFormsTemplates = $pathToTemplates . '/forms/';
+	    if (file_exists($pathToFormsTemplates . 'form.php')) {
+		    include_once $pathToFormsTemplates . 'form.php';
+	    } ?>
+    </div>
