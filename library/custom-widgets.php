a5ea98bddfd400387011fe3255c6580021fd8be4
brownrabbit0207 <brownrabbit0207@gmail.com>
2020-10-31 00:00:00 +0900

    "2020-10-31 00:00"

--- library/custom-widgets.php
+++ library/custom-widgets.php
@@ -18,6 +18,27 @@ class WP_Categories_Widget extends WP_Widget {
         render_widget_form($instance, $this->get_field_id( 'title' ), $this->get_field_name( 'title' ), 'Categories');
     }
 
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+        return $instance;
+    }
+}
+
+// Add product categories widget for WP >= 5.8
+class WP_ProductCategories_Widget extends WP_Widget {
+
+    function __construct() {
+        parent::__construct(
+            'WP_ProductCategories_Widget',
+            __('Product Categories', 'default'),
+            array( 'description' => __( 'A list of product categories.', 'default' ), )
+        );
+    }
+
+    public function widget( $args, $instance ) {
+        render_custom_widget($args, $instance , 'WC_Widget_Product_Categories');
+    }
 
     public function form( $instance ) {
         render_widget_form($instance, $this->get_field_id( 'title' ), $this->get_field_name( 'title' ), 'Product Categories');
