d2c9e9480cdd285f36fd7121c018f1f79890f50b
brownrabbit0207 <brownrabbit0207@gmail.com>
2021-01-14 00:00:00 +0900

    "2021-01-14 00:00"

--- library/tgm-activation.php
+++ library/tgm-activation.php
@@ -8,6 +8,17 @@ function theme_register_required_plugins() {
 	$plugin_source = get_template_directory() . '/plugins/nicepage.zip';
 	if (!file_exists($plugin_source)) {
 		// noting to install
+		return;
+	}
+
+	$plugins = array(
+      		array(
+      			'name'               => 'Nicepage',     // The plugin name.
+      			'slug'               => 'nicepage',     // The plugin slug (typically the folder name).
+      			'source'             => $plugin_source, // The plugin source.
+      			'required'           => false,          // If false, the plugin is only 'recommended' instead of required.
+      			'version'            => '',             // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
+      			'force_activation'   => false,          // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
       			'force_deactivation' => false,          // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
       			'external_url'       => '',             // If set, overrides default API URL and points to an external URL.
       			'is_callable'        => '',             // If set, this callable will be be checked for availability to determine if a plugin is active.
