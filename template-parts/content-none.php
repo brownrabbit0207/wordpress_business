84955b43bd966fa53322884b55e2fab1861eaa59
brownrabbit0207 <brownrabbit0207@gmail.com>
2021-02-04 00:00:00 +0900

    "2021-02-04 00:00"

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * The template part for displaying a message that posts cannot be found
  */
@@ -12,19 +13,3 @@
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'website4829605' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'website4829605' ); ?></p>
-            </br>
-            <?php get_search_form(); ?>
-            </br>
-
-		<?php else : ?>
-
-			<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'website4829605' ); ?></p>
-			<?php get_search_form(); ?>
-
-		<?php endif; ?>
-	</div><!-- .page-content -->
-</section><!-- .no-results -->
