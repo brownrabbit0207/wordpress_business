9cef128b733b5a1111b6ded446f35f5c8e2217e2
brownrabbit0207 <brownrabbit0207@gmail.com>
2021-03-17 00:00:00 +0900

    "2021-03-17 00:00"

--- template-parts/postTemplate/single-content.php
+++ template-parts/postTemplate/single-content.php
@@ -8,17 +8,6 @@
                             $post_image = theme_get_post_image(array('class' => 'u-blog-control u-expanded-width u-image u-image-default u-image-1', 'default' => '/images/0fd3416c.jpeg'));
                             if ($post_image) echo $post_image; else { echo '<div class="none-post-image" style="display: none;"></div>'; $skip_min_height = true; } ?><!--/blog_post_image--><!--blog_post_header-->
         <h2 class="u-blog-control u-text u-text-1">
-          <?php if (!is_singular()): ?><a class="u-post-header-link" href="<?php the_permalink(); ?>"><?php endif; ?><?php the_title(); ?><?php if (!is_singular()): ?></a><?php endif; ?>
-        </h2><!--/blog_post_header--><!--blog_post_metadata-->
-        <div class="u-blog-control u-metadata u-metadata-1"><!--blog_post_metadata_date-->
-          <span class="u-meta-date u-meta-icon"><?php if(is_search() || $post->post_type !== "page"){ echo get_the_date(); }?></span><!--/blog_post_metadata_date--><!--blog_post_metadata_category-->
-          <?php $categories = theme_get_category_list(); if ($categories):?><span class="u-meta-category u-meta-icon"><?php echo $categories; ?></span><?php endif; ?><!--/blog_post_metadata_category--><!--blog_post_metadata_comments-->
-          <?php if (comments_open()): ?><span class="u-meta-comments u-meta-icon"><a class="u-textlink" href="<?php echo get_comments_link(); ?>">
-                                        <?php _e(sprintf(__('Comments (%d)', 'website4829605'), (int)get_comments_number())); ?>
-                                   </a></span><?php endif; ?><!--/blog_post_metadata_comments-->
-        </div><!--/blog_post_metadata--><!--blog_post_content-->
-        <div class="u-align-justify u-blog-control u-post-content u-text u-text-2"><?php echo !is_search() && (is_singular() || $post->post_type !== 'post') ? theme_get_content() : theme_get_excerpt(); ?></div><!--/blog_post_content-->
-      </div>
     </div><!--/blog_post--><!--/post_details-->
   </div>
 </section><?php if ($skip_min_height) { echo "<style> .u-section-1, .u-section-1 .u-sheet {min-height: auto;}</style>"; } ?>
