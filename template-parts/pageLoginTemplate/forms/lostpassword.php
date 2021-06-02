1cd6dbc02cdcc113203da37b0c85bc08d8b68c0c
brownrabbit0207 <brownrabbit0207@gmail.com>
2021-06-03 00:00:00 +0900

    "2021-06-03 00:00"

--- template-parts/pageLoginTemplate/forms/lostpassword.php
+++ template-parts/pageLoginTemplate/forms/lostpassword.php
@@ -8,3 +8,14 @@
         <div class="u-align-left u-form-group u-form-submit">
           <a href="#" class="u-btn u-btn-submit u-button-style u-btn-1"><?php _e( 'Get New Password' ); ?></a>
           <input type="submit" value="submit" class="u-form-control-hidden">
+        </div>
+        <input type="hidden" value="" name="recaptchaResponse">
+      </form>
+<?php
+$pathToLinkTemplates = get_template_directory() . '/template-parts/' . $pageLogin_custom_template . '/links/';
+if (file_exists($pathToLinkTemplates . 'login.php')) {
+	include_once $pathToLinkTemplates . 'login.php';
+}
+if (file_exists($pathToLinkTemplates . 'register.php')) {
+	include_once $pathToLinkTemplates . 'register.php';
+}
