e3bd0c36a22fe4919555a95f4665dcab5cd7d9c1
brownrabbit0207 <brownrabbit0207@gmail.com>
2021-07-14 00:00:00 +0900

    "2021-07-14 00:00"

--- template-parts/content-none.php
+++ template-parts/content-none.php
@@ -3,16 +3,11 @@
  * The template part for displaying a message that posts cannot be found
  */
 ?>
-	<div class="page-content">
-		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
-			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'website4829605' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
-
-		<?php elseif ( is_search() ) : ?>
-
-			<p><?php _e( 'Sorry, but nothing matched your search terms. Please try again with some different keywords.', 'website4829605' ); ?></p>
-            </br>
-            <?php get_search_form(); ?>
+<section class="no-results not-found">
+	<header class="page-header">
+		<h2 class="page-title"><?php _e( 'Nothing Found', 'website4829605' ); ?></h2>
+	</header><!-- .page-header -->
             </br>
 
 		<?php else : ?>
