d7de062cd0f8875714c2d7e8a40fd9d6a80e81ee
brownrabbit0207 <brownrabbit0207@gmail.com>
2021-08-11 00:00:00 +0900

    "2021-08-11 00:00"

--- template-parts/footer-styles.php
+++ template-parts/footer-styles.php
@@ -8,17 +8,6 @@
 }
 .u-footer .u-text-1 {
   width: 417px;
-  margin: 49px auto;
-}
-@media (max-width: 1199px) {
-  .u-footer .u-sheet-1 {
-    min-height: 99px;
-  }
-}
-@media (max-width: 991px) {
-  .u-footer .u-sheet-1 {
-    min-height: 76px;
-  }
 }
 @media (max-width: 767px) {
   .u-footer .u-sheet-1 {
